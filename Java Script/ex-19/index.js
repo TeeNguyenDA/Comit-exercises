8b4f0669b3667c96bbc42636ea834158bc10cbe1
ThanhNguyen-Tanny <thanhnguyen.ulaval@gmail.com>
2020-09-23 15:44:59 -0400

    Added ex 18-20

---
+++ Java Script/ex-19/index.js
@@ -0,0 +1,21 @@
+/*Variables with initial values*/
+let monthOfBirth = "October";
+let firstName = "Thanh";
+let lastName = "Nguyen";
+
+/*Variables without initial values*/
+let city, age, nextCity;
+city = "Paris";
+age = "20";
+nextCity = city;
+
+/*Console.log all values*/
+console.log("monthOfBirth: ", monthOfBirth);
+console.log("firstName: ", firstName);
+console.log("lastName: ", lastName);
+console.log("city: ", city);
+console.log("age ", age);
+console.log("nextCity: ", city);
+
+/*Log first name, last name combined by backticks*/
+console.log(`${firstName}, ${lastName}`)
