ddd062e33a44b3acfc47c5c73aa1b3e7141e9f45
ThanhNguyen-Tanny <thanhnguyen.ulaval@gmail.com>
2020-09-30 16:37:33 -0400

    Refixed the bugs w existing comments

--- Java Script/index.js
+++ Java Script/index.js
@@ -5,85 +5,43 @@ for (num = 0; num <= 1000; num ++) {
     sum += num;//or sum = sum + num;
     if (sum > 400) break;
 }
-alert("Sum: " + sum);//or console.log("Sum is", sum)
+console.log("Sum: " + sum);//or console.log("Sum is", sum)
 
 /*Loops: Show numbers from 0 to 10*/
+let currentNumber = 0;
 for (let currentNumber = 0; currentNumber < 10; currentNumber++) {
     alert (`Number ${currentNumber}`);
 }
-console.log ("currentNumber: ", currentNumber)
+console.log ("currentNumber: ", currentNumber);
 
 /*Loops: Show numbers from 100 to 0*/
 for (let currentNumber = 100; currentNumber > 0; currentNumber--) {
-    alert (`Number ${currentNumber}`);
 }
-console.log ("currentNumber: ", currentNumber)
+console.log ("currentNumber: ", currentNumber);
 
 /*Loops: Show even numbers between 0 and 100*/
 for (let currentNumber = 0; currentNumber < 100; currentNumber++) {
     if ((currentNumber - 1) % 2) {
-        alert (`Number ${currentNumber}`);
     }
 }
 console.log ("currentNumber: ", currentNumber)
 
 /*Loops: With the mentioned output*/
-let row = "";
-for(let row =1; row <=13; row++) {
+for(let row = 1; row <=13; row++) {
+  let star = "";
   
-  for(let star=1; star <=row; row++) {
-    star += "*";
+  for(let star = 1; star <= row; star++) {
+    star = star + "*"; //Or star = star + "*"
   }
-  star += "\n";
+console.log (star);
 }
-console.log(star);
 // Other ways: let stars=""; for (let count=1; count<=13; count++){ stars+="*"; console.log(stars);}//
 
-let lineNumber = 1;
-while (lineNumber <= 13) {
-    let stars = "";
-    while (stars <= lineNumber) {
-        stars = stars + "*";
-        starCount++
+for(let row = 1; row <=13; row++) {
+    let star = "";
+    
+    for(let starCount = 1; starCount <= row; starCount++) {
+      star += "*"; //Or star = star + "*"
     }
-    lineNumber++;
-}
-console.log(stars);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for (repeatedString = 1; repeatedString <= 13; repeatedString++) {
-    alert (`${totalStarNumber}`);
-}
+  console.log (star);
+  }
